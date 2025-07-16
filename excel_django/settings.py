33b0f29d0c3ce8817c91222b3e441529b155d6ff
Roberto SolísG <solisgroberto@gmail.com>
2025-07-16 16:33:04 -0600

    corregi host para produción

--- excel_django/settings.py
+++ excel_django/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-ucc3l77_#^9bo#myl9tm712c8f9_x42p3k%3)@p+ar6wm+y7-f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-##ALLOWED_HOSTS = ['207.249.96.155', 'localhost']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['207.249.96.155', 'localhost']
+#ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -130,10 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-##STATICFILES_DIRS = [
-##    BASE_DIR / "static"
-##    
-##] #se agrega para conf. archivo estático JSON
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+    
+] #se agrega para conf. archivo estático JSON
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
